d70a775939d993b1d05bf24c3fc9d8a99d0a258f
Student <example@example.com>
2019-04-04 16:28:12 -0700

    fix error and returns correct result with words starting with consonants

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
 // business logic
 var vowels = ["a", "e", "o", "u", "i", "A", "E", "I", "O", "U"]
-var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s" , "t", "v", "w", "x", "y", "z", "B", "V", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"]
+// var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s" , "t", "v", "w", "x", "y", "z", "B", "V", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"]
 
 function translateWord(userInput) {
-
   var userInput = "farts";
   var userInputArray = userInput.split("");
   console.log(userInput);
@@ -20,13 +19,11 @@ function translateWord(userInput) {
     stop++;
   }
   var output = userInputArray.slice(stop).join("") + userInputArray.slice(0, stop).join("") + "ay";
+  return output;
   //this line does what we tried below
   // var moveConsonents = userInputArray.slice(0, stop);
   // var newUserInputArray =
   // userInputArray.join("") + moveConsonents + "ay";
-  $(".pigLatinResult").text(output);
-  $("#positiveResult").show();
-  console.log(output);
 }
 
 
@@ -36,8 +33,11 @@ $(document).ready(function() {
     event.preventDefault();
     var userInput = $("#userInput").val();
     var userInputArray = userInput.split("");
+    var output = translateWord(userInput);
     // var result = pigLatinWord(userInput);
     // console.log(userInput, userInputArray)
-
+    $(".pigLatinResult").text(output);
+    $("#positiveResult").show();
+    console.log(output);
   });
 });
